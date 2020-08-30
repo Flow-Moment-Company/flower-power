26c4d1339a18922af7cfce683407087e1a2da881
Matthew Hughes <matthewhughes@pointstire.com>
2020-08-29 18:48:33 -0700

    Moved nav to top bar

--- src/components/core/header.vue
+++ src/components/core/header.vue
@@ -1,33 +1,21 @@
 <template>
-    <div>
-        <v-app-bar color="primary">
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-            <v-spacer />
-            <v-btn v-if="loggedIn" @click="fcl.unauthenticate">logout</v-btn>
-            <v-btn v-else @click="fcl.authenticate">login</v-btn>
-        </v-app-bar>
-        <v-navigation-drawer v-model="drawer" app>
-            <v-list nav>
-                <v-list-item exact :to="{ name: 'Home' }">
-                    <v-list-item-icon>
-                        <v-icon>mdi-home</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>Home</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-
-                <v-list-item v-if="loggedIn" exact :to="{ name: 'Account' }">
-                    <v-list-item-icon>
-                        <v-icon>mdi-account</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>Account</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-    </div>
+    <v-toolbar color="primary" app>
+        <v-toolbar-items class="hidden-xs-only">
+            <v-btn text to="/" color="white">
+                <v-icon left>mdi-home</v-icon>Home
+            </v-btn>
+            <v-btn text to="/account" color="white" v-if="loggedIn">
+                <v-icon left>mdi-account</v-icon>Account
+            </v-btn>
+        </v-toolbar-items>
+        <v-spacer />
+        <v-btn text color="white" v-if="loggedIn" @click="fcl.unauthenticate">
+            <v-icon left>mdi-logout</v-icon>Logout
+        </v-btn>
+        <v-btn text color="white" v-else @click="fcl.authenticate">
+            <v-icon left>mdi-login</v-icon>Login
+        </v-btn>
+    </v-toolbar>
 </template>
 
 <script>
@@ -38,7 +26,6 @@ export default {
     data() {
         return {
             fcl,
-            drawer: true,
         };
     },
     computed: {
